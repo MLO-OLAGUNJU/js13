550a349afdcc0821253d43ddb128cb61ce6c9f58
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-18 19:29:41 +0100

    learning javascript functions

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Function
+
+//Methods = built in Fucntions!
+/* 
+"Manny".toLowerCase(); // toLowercase() is a function
+Math.random(); // another built in fucntion */
+
+//Function declaration syntax:
+/* function sum(num1, num2) {
+  if (num2 === undefined) {
+    return num1 + num1;
+  }
+  if (num1 === undefined) {
+    return num2 + num2;
+  }
+  return num1 + num2;
+}
+console.log(sum(90, 10)); */
+
+/* function getUsernameFromEmail(email) {
+  return email.slice(0, email.indexOf("@"));
+}
+console.log(getUsernameFromEmail("user@GitHub.com")); */
+
+function name(params) {}
